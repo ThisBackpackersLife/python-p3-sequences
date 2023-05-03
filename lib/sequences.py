03f947b4424d39636b75d2199769bd9ededdff41
Nicholas <crazedglitch@yahoo.com>
2023-05-03 14:35:51 -0500

    lab complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci( length ):
+    fibonacci_list = [ 0,1,1,2,3,5,8,13,21,34,55,89 ]
+    print( fibonacci_list[ 0:length ])
